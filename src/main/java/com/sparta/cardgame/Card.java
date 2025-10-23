15c7240bf81a3c2d530bd5e2168d233432acbf69
Leda909 <ko9zad@gmail.com>
2025-10-23 12:31:58 +0100

    set up fields

--- src/main/java/com/sparta/cardgame/Card.java
+++ src/main/java/com/sparta/cardgame/Card.java
@@ -1,15 +1,35 @@
 package com.sparta.cardgame;
 
 public class Card {
-    String suit; // Diamonds
-    String name; // Ace, 2, 9, Jack
-    int value; // 2 - 11
+//    String suit; // Diamonds
+//    String name; // Ace, 2, 9, Jack
+//    int value; // 2 - 11
+    public enum Suit{
+        Clubs,Diamonds,Hearts,Spades
+    }
+    public enum Name {
+        ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
+    }
+
+    private final Suit suit;
+    private final Name name;
 
     //constructor Card (suit, name) name -> value
+    public Card(Suit suit, Name name) {
+        this.suit = suit;
+        this.name = name;
+    }
 
     // method getFormattedName () -> name + " of " suit
     // method getShortName () -> name[0] + suit[0] ???
+
     // method getValue () -> value
+    public int getValue(){
+        if (name.ordinal() >= 9){
+            return 10;
+        }
+        return name.ordinal()+1;
+    };
 
     // method getName () -> name SNAP EXTENSION
 }
